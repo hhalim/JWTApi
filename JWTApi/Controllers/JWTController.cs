275400a1843000bd6da42c7985b3dd9a363fcca4
Hartono Halim <ah3555@Halff.com>
2019-01-31 08:19:51 -0600

    first commit

---
+++ JWTApi/Controllers/JWTController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWTApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class JWTController : Controller
+    {
+        // GET: api/<controller>
+        [HttpPost]
+        [Route("[action]")]
+        [Authorize]
+        public bool Test([FromBody] string message)
+        {
+            return true;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Generate()
+        {
+            var secKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("%D*G-KaPdSgUkXp2"));
+            var creds = new SigningCredentials(secKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "Halff.com",
+                audience: "Halff.com",
+                signingCredentials: creds);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
+    }
+}
---
+++ JWTApi/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                   .AddJwtBearer(options =>
+                   {
+                       options.TokenValidationParameters = new TokenValidationParameters
+                       {
+                           ValidateLifetime = false,
+                           ValidateIssuer = true,
+                           ValidateAudience = true,
+                           ValidateIssuerSigningKey = true,
+                           ValidIssuer = "Halff.com",
+                           ValidAudience = "Halff.com",
+                           IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("%D*G-KaPdSgUkXp21234"))
+                           //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("%D*G-KaPdSgUkXp2"))
+                       };
+                   });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
